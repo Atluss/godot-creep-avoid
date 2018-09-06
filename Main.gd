6da9e5ed99d87de1e8f3ef604856b09713581ce0
Shokin Ilya <atlus3@mail.ru>
2018-09-06 12:38:54 +0300

    finish

--- Main.gd
+++ Main.gd
@@ -20,13 +20,19 @@ func _ready():
 
 
 func game_over():
-    $ScoreTimer.stop()
-    $MobTimer.stop()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
-    score = 0
-    $Player.start($StartPosition.position)
-    $StartTimer.start()
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 
 func _on_MobTimer_timeout():
@@ -49,6 +55,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 	pass # replace with function body
 
 
@@ -56,3 +63,4 @@ func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
 	pass # replace with function body
+
